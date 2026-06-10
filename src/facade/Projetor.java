c0fcdffa35183f0e29243a3ea7e9af44579fd6a6
Guilherme Fiuza <guifportela@gmail.com>
2026-06-10 12:56:23 -0300

    feat: implementação do padrão Facade

---
+++ src/facade/Projetor.java
@@ -0,0 +1,11 @@
+package facade;
+
+public class Projetor {
+
+    public void ligar(){
+        System.out.println("Projetor ligado.");
+    }
+    public void desligar(){
+        System.out.println("Projetor desligado.");
+    }
+}
